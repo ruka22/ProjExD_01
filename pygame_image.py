ceae4204d5bb13d5c11c221265f321ada793b789
c0b22016c1 <c0b22016c1@edu.teu.ac.jp>
2023-04-18 16:48:00 +0900

    練習問題6

--- pygame_image.py
+++ pygame_image.py
@@ -16,7 +16,9 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [1600-tmr, 0])
+        print(tmr)
 
         n = tmr % 2
         screen.blit(kk_imgs[n],[300,200])
