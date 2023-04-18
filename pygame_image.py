1747791e508d9c73fc9b28c0cbea3c7103682cc0
c0b22016c1 <c0b22016c1@edu.teu.ac.jp>
2023-04-18 18:28:19 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -7,7 +7,18 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230418/fig/pg_bg.jpg")
     kk_img = pg.transform.flip(pg.image.load("ex01-20230418/fig/3.png"),True,False)
-    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img,10,1.0)] 
+    kk_imgs = [kk_img, \
+               pg.transform.rotozoom(kk_img,0.5,1.0), pg.transform.rotozoom(kk_img,1,1.0), \
+               pg.transform.rotozoom(kk_img,1.5,1.0), pg.transform.rotozoom(kk_img,2,1.0), \
+               pg.transform.rotozoom(kk_img,2.5,1.0), pg.transform.rotozoom(kk_img,3,1.0), \
+               pg.transform.rotozoom(kk_img,3.5,1.0), pg.transform.rotozoom(kk_img,4,1.0), \
+               pg.transform.rotozoom(kk_img,4.5,1.0), pg.transform.rotozoom(kk_img,5,1.0), \
+               pg.transform.rotozoom(kk_img,5.5,1.0), pg.transform.rotozoom(kk_img,5,1.0), \
+               pg.transform.rotozoom(kk_img,4.5,1.0), pg.transform.rotozoom(kk_img,4,1.0), \
+               pg.transform.rotozoom(kk_img,3.5,1.0), pg.transform.rotozoom(kk_img,3,1.0), \
+               pg.transform.rotozoom(kk_img,2.5,1.0), pg.transform.rotozoom(kk_img,2,1.0), \
+               pg.transform.rotozoom(kk_img,1.5,1.0), pg.transform.rotozoom(kk_img,1,1.0), \
+               pg.transform.rotozoom(kk_img,0.5,1.0)] 
 
     tmr = 0
 
@@ -16,11 +27,12 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img, [1600-tmr, 0])
-        print(tmr)
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(pg.transform.flip(bg_img,True,False), [1600-x, 0])
+        screen.blit(bg_img,[3200-x,0])
 
-        n = tmr % 2
+        n = tmr % 22
         screen.blit(kk_imgs[n],[300,200])
 
         pg.display.update()
