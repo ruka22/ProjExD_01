478603ba382fef0467edb2bc31f5d1c8cd489471
c0b22016c1 <c0b22016c1@edu.teu.ac.jp>
2023-04-18 15:54:58 +0900

    練習問題１

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01-20230418/fig/pg_bg.jpg")
+    kk_img = pg.transform.flip(pg.image.load("ex01-20230418/fig/3.png"),True,False)
+    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img,10,1.0)] 
+
+    tmr = 0
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        tmr += 1
+        screen.blit(bg_img, [0, 0])
+
+        pg.display.update()
+        clock.tick(100)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
